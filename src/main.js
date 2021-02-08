05f8e85200c1bb90ef7434ab0d9ab99a3ab58b52
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-02-08 16:49:36 -0600

    update prod backend url again

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import axios from "axios";
 
 
 axios.defaults.baseURL = 
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://hidden-dawn-34584.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://bidcast.herokuapp.com/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
